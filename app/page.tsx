b4620044d56a53937e13c8c1e5852c176452093b
hassan <hassanmohamed6102006@gmail.com>
2025-07-02 17:19:11 +0300

    Update and rename page.ts to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,3 +2,9 @@ export const metadata = {
   title: "منصة المدرسين | Hassan Platform",
   description: "أفضل منصة للبحث عن مدرسين لكل المراحل التعليمية",
 };
+
+import HomePageClient from "./HomePageClient";
+
+export default function HomePage() {
+  return <HomePageClient />;
+}
